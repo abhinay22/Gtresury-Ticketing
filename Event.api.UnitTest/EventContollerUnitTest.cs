5058ee4cca403e417b87e69ec8f84c7eb708e250
Abhinay Arora <aroraabhinay@gmail.com>
2025-04-02 21:00:32 +0100

    test whether controller call createEvent service method in Action method

---
+++ Event.api.UnitTest/EventContollerUnitTest.cs
@@ -0,0 +1,42 @@
+namespace Event.api.UnitTest;
+
+using Event.api.Controllers;
+using EventService;
+using EventService.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+
+public class EventContollerUnitTest
+{
+    private  Mock<IEventService> svc;
+
+    private EventController _contoller;
+
+    private Mock<ILogger<EventController>> logger;
+
+    public EventContollerUnitTest()
+    {
+       svc = new Mock<IEventService>();
+
+        logger = new Mock<ILogger<EventController>>();
+
+        _contoller = new EventController(svc.Object,logger.Object);
+
+       
+
+    }
+
+    [Fact]
+    // checks if the service layer is called from CreateActionContrtoller
+    public void CreateEventController_Calls_ServicelayerMethod()
+    {
+       CreatedAtActionResult result=(CreatedAtActionResult)_contoller.CreateEvent(null);
+
+        svc.Setup(y => y.CreateEvent(It.IsAny<CreateEventDTO>())).Returns(true);
+
+        svc.Verify(x=>x.CreateEvent(It.IsAny<CreateEventDTO>()),Times.Once);
+
+    }
+}
